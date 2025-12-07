4b1b736cb1ffc2f176832fb99f8b243f48cb664a
NabeelManjhoti <nabeelalimanjhoi@gmail.com>
2025-12-07 15:45:59 +0500

    deployment process

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://NabeelManjhoti.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/GIAIC-Hackathon-I-Physical-AI-Book',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'your-username', // Usually your GitHub org/user name.
+  organizationName: 'NabeelManjhoti', // Usually your GitHub org/user name.
   projectName: 'GIAIC-Hackathon-I-Physical-AI-Book', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
 
